88d2bec492fc48adacb668ab8bb6821ece0a3c89
Ananya <ananya22112@gmail.com>
2026-03-31 09:04:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.1 MB (97.68%)

---
+++ 0812-rotate-string/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if(len(s)!=len(goal)):
+            return False
+        s1=s+s
+        if goal in s1:
+            return True
+        return False
