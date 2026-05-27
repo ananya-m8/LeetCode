895b6e483e00dcc12423912bcbc89f48d716765e
Ananya <ananya22112@gmail.com>
2026-05-27 15:20:07 +0000

    Sync LeetCode submission Runtime - 1177 ms (89.63%), Memory - 19.3 MB (62.15%)

---
+++ 1890-sum-of-beauty-of-all-substrings/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def beautySum(self, s: str) -> int:
+        n=len(s)
+        total=0
+        for i in range(n):
+            freq={}
+            for j in range(i,n):
+                freq[s[j]]=freq.get(s[j],0)+1
+                values=freq.values()
+                mini=min(values)
+                maxi=max(values)
+                total+=(maxi-mini)
+        return total
