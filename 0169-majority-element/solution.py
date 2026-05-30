f37e9f4f891e6614ce8329acf5a1baa75e684d6a
Ananya <ananya22112@gmail.com>
2026-05-30 07:00:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.1 MB (48.55%)

--- 0169-majority-element/solution.py
+++ 0169-majority-element/solution.py
@@ -1,9 +1,4 @@
-import math
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        d = {}
-        for i in nums:
-            if(i not in d and nums.count(i)>math.floor(len(nums)/2)):
-                return i
-            else:
-                d[i] = 1
+        nums.sort()
+        return nums[len(nums)//2]
