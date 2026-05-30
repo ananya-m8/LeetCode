4ab6c14d277c892c6f76b02661738cc1aa86de9c
Ananya <ananya22112@gmail.com>
2026-05-30 08:40:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (60.40%)

---
+++ 0172-factorial-trailing-zeroes/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        c=0
+        if(n<5):
+            return 0
+        while(n>0):
+            n=n//5
+            c+=n
+        return c
