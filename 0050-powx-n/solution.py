83e78c77619606bdb43e5e676efaea50e1780926
Ananya <ananya22112@gmail.com>
2026-06-09 14:39:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (85.76%)

---
+++ 0050-powx-n/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if(n<0):
+            return 1.0/self.power(x,-n)
+        return self.power(x,n)
+    def power(self,x,n):
+        if n==0:
+            return 1
+        if n%2==0:
+            return self.power(x*x,n//2)
+        return x*self.power(x,n-1)
+    
