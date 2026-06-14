c6e28689d43097245495693f61913f20c0f58ea2
Ananya <ananya22112@gmail.com>
2026-06-14 06:06:23 +0000

    Sync LeetCode submission Runtime - 3 ms (60.79%), Memory - 21.3 MB (29.22%)

--- 0136-single-number/solution.py
+++ 0136-single-number/solution.py
@@ -1,8 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        sum1 = sum(nums)
-        sum2 = 0
-        set1 = set(nums)
-        for i in set1:
-            sum2+=i
-        return 2*sum2-sum1
+        xorr=0
+        for i in nums:
+            xorr=xorr^i
+        return xorr
