c62992a05bce0cc290223a46f24ef72ccf1d8091
Ananya <ananya22112@gmail.com>
2026-06-17 14:42:38 +0000

    Sync LeetCode submission Runtime - 27 ms (43.00%), Memory - 20.9 MB (86.81%)

---
+++ 0503-next-greater-element-ii/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        l=nums+nums
+        n=len(nums)
+        res=[-1]*n
+        stack=[]
+        for i in range(2*n-1,-1,-1):
+            ind=i%n
+            cur=nums[ind]
+            while stack and stack[-1]<=l[i]:
+                stack.pop()
+            if i<n:
+                if stack:
+                    res[i]=stack[-1]
+            stack.append(l[i])
+        return res
