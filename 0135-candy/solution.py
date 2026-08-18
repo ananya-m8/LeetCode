ef228433f07edcb098f99ebebe8ac1a40d4c80b6
Ananya <ananya22112@gmail.com>
2026-08-18 10:53:28 +0000

    Sync LeetCode submission Runtime - 53 ms (7.15%), Memory - 25.6 MB (5.79%)

--- 0135-candy/solution.py
+++ 0135-candy/solution.py
@@ -1,20 +1,11 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n=len(ratings)
-        candy=n
-        i=1
-        while i<n:
-            if ratings[i]==ratings[i-1]:
-                i+=1
-            peak=0
-            while i<n and ratings[i]>ratings[i-1]:
-                peak+=1
-                candy+=peak
-                i+=1
-            valley=0
-            while i<n and ratings[i]<ratings[i-1]:
-                valley+=1
-                candy+=valley
-                i+=1
-            candy-=min(peak,valley)
-        return candy
+        candy=[1]*n
+        for i in range(1,n):
+            if ratings[i]>ratings[i-1]:
+                candy[i]=candy[i-1]+1
+        for j in range(n-2,-1,-1):
+            if ratings[j]>ratings[j+1]:
+                candy[j]=max(candy[j],candy[j+1]+1)
+        return sum(candy)
