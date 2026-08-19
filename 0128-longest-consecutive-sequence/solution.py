15b3b80e15869621792b0fdc1cbde00af7e6c46d
Ananya <ananya22112@gmail.com>
2026-08-19 13:23:47 +0000

    Sync LeetCode submission Runtime - 43 ms (85.20%), Memory - 36.6 MB (66.45%)

--- 0128-longest-consecutive-sequence/solution.py
+++ 0128-longest-consecutive-sequence/solution.py
@@ -8,11 +8,9 @@ class Solution:
         count=1
         for i in range(1,len(nums)):
             j.append(nums[i]-nums[i-1])
-            print(j[i-1])
             if(j[i-1]!=1):
                 if(m<count):
                     m=count
-                    print(m)
                 count=1
             else:
                 count+=1
