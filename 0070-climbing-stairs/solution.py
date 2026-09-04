2e121697407107285ceca17a39f6a892dee12d73
Ananya <ananya22112@gmail.com>
2026-09-04 13:38:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (86.89%)

---
+++ 0070-climbing-stairs/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n==0 or n==1:
+            return 1
+        prev,cur=1,1
+        for i in range(2,n+1):
+            temp=cur
+            cur=prev+cur
+            prev=temp
+        return cur
