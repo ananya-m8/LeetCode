5142f45b089da710b9b448957aebb2fa410773b0
Ananya <ananya22112@gmail.com>
2026-09-05 05:26:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.8 MB (42.67%)

---
+++ 0337-house-robber-iii/solution.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def dfs(node):
+            if not node:
+                return [0,0]
+            left=dfs(node.left)
+            right=dfs(node.right)
+            notRob=max(left)+max(right)
+            rob=node.val+left[0]+right[0]
+            return [notRob,rob]
+        res=dfs(root)
+        return max(res)
