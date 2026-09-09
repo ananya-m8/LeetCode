592f273df442a91e516a219ee32b37b003dfe18a
Ananya <ananya22112@gmail.com>
2026-09-09 13:58:05 +0000

    Sync LeetCode submission Runtime - 4 ms (61.36%), Memory - 19.4 MB (23.01%)

--- 0205-isomorphic-strings/solution.py
+++ 0205-isomorphic-strings/solution.py
@@ -1,5 +1,14 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        zipped_set = set(zip(s, t))
-        return len(zipped_set) == len(set(s)) == len(set(t))
-        
+        if len(s)!=len(t):
+            return False
+        d1={}
+        n=len(s)
+        for i in range(n):
+            if s[i] in d1:
+                if d1[s[i]]!=t[i]:
+                    return False
+            elif t[i] in d1.values():
+                return False
+            d1[s[i]]=t[i]
+        return True
